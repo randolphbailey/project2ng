a7e7f60938bb2f1c1b2c8e148e486665bb7d01f0
janelw <51757148+janelw@users.noreply.github.com>
2019-11-18 08:18:21 -0600

    login works, returns homepage under view /home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import {GlobalVariablesService} from "src/app/services/globalVariables/global-variables.service";
 
 @Component({
   selector: "app-home",
@@ -6,7 +7,14 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(    private globalvariableService: GlobalVariablesService,
+    ) {
 
-  ngOnInit() {}
+    }
+
+  ngOnInit() {
+    console.log(this.globalvariableService.getCurrentUser());
+  }
+
+
 }
--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,14 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { ControlMessagesComponent } from 'src/app/components/register/control-messages.component';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterTestingModule } from '@angular/router/testing';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent, ControlMessagesComponent],
+      imports: [ReactiveFormsModule, HttpClientModule, RouterTestingModule ]
     })
     .compileComponents();
   }));
