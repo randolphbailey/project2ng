0f928d2e90afb36a5d1c690dc8dd7e894dafa462
janelw <51757148+janelw@users.noreply.github.com>
2019-11-20 09:10:23 -0600

    Dashboard works

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,34 +3,69 @@ import {GlobalVariablesService} from "src/app/services/globalVariables/global-va
 import { JradUser } from 'src/app/models/JradUser';
 import { Post } from 'src/app/models/Post';
 import { PostService } from 'src/app/services/post/post.service';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { JradUserService } from 'src/app/services/jradUser/jrad-user.service';
+import {Comment} from 'src/app/models/Comment';
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-
+  commentForm: FormGroup;
   user: JradUser;
-  post: Post[] = [];
+  allpost: Post[] = [];
   currentuser: string;
+  commenter: JradUser;
+  comment: Comment;
+  post
   constructor(    private globalvariableService: GlobalVariablesService,
-                  private ps: PostService) {
+                  private ps: PostService,
+                  private fb: FormBuilder,
+                  private jr: JradUserService) {
 
     }
 
   ngOnInit() {
     this.user = this.globalvariableService.getCurrentUser();
-    this.currentuser = this.user.username;
-    this.ps.getPostByUser("test").subscribe(
+    // this.currentuser = this.user.username;
+    console.log(this.user);
+    this.ps.getAllPosts().subscribe(
       data => {
-      this.post = [];
-      this.post = data;
+      this.allpost = [];
+      this.allpost = data;
+      console.log(this.allpost);
       },
       error => console.log(error)
       );
+    this.commentForm = this.fb.group({
+        content: ["", Validators.maxLength(300)],
+      });
 
-  }
+    this.jr.getUserByUsername(this.user.username).subscribe(
+      data =>{
+        this.commenter = data;
+        console.log(this.commenter);
+      });
+    }
+
+
+  // onSubmit() {
+  //   if(this.commentForm.valid){
+  //   this.comment = new Comment(
+  //     0,
+  //     this.commentForm.value.content,
+  //     0,
+  //     0,
+  //     this.commenter,
+
+
+
+  //   );
+  //   }
+
+  // };
 
 
 }
+
